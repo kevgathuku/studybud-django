c20dfbaf70d2f4cdb3b5210653761386e679fd81
Kevin Gathuku <kevgathuku@gmail.com>
2022-07-31 09:11:46 +0300

    Add initial room model / migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    # host
+    # topic
+    name = models.CharField(max_length=200)
+    # null is for the DB. Blank allows the form field to be empty
+    description = models.TextField(null=True, blank=True)
+    # participants
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
