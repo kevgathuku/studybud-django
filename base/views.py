d88ac24a2f33f827053c0e6ddea6f2b88bcc1078
Kevin Gathuku <kevgathuku@gmail.com>
2022-07-31 08:39:30 +0300

    Add dynamic data to templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Learn Design'},
+    {'id': 3, 'name': 'Frontend Devs Assemble'},
+]
+
+
 def home(request):
-  return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
+
 
 def room(request):
-  return render(request, 'room.html')
+    return render(request, 'room.html')
