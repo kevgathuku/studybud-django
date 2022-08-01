54e258539e1f2f26fafd785beba9a37e681bdc96
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-01 07:04:05 +0300

    Add topic and message models

--- base/models.py
+++ base/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
-    # host
-    # topic
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     # null is for the DB. Blank allows the form field to be empty
     description = models.TextField(null=True, blank=True)
@@ -13,3 +21,15 @@ class Room(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # CASCADE - Delete messages when the associated Room is deleted
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.body[0:50]
