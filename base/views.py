ec5a720f45f5c0c4565eafea36a30c565d4f7cf4
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-01 12:27:59 +0300

    Redirect logged in users to home

--- base/views.py
+++ base/views.py
@@ -10,6 +10,9 @@ from .forms import RoomForm
 
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
